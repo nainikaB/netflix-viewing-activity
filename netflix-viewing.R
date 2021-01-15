262a08d0f106f747fe76adce0e550374f041c1c8
Nainika Baliga <nainika@Nainikas-MacBook-Air.local>
2021-01-16 01:00:31 +0530

    add graph for shows watched on days

--- netflix-viewing.R
+++ netflix-viewing.R
@@ -4,12 +4,13 @@ library(tidyr)
 library(lubridate)
 library(zoo)
 library(ggplot2)
+library(magrittr)
 
 # Reading data from downloaded CSV file of my netflix viewing activity 
 netflixactivity <- read.csv("NetflixViewingHistory.csv")
 netflixactivity
 str(netflixactivity)
-netflixactivity$Date <- dmy(netflixactivity$Date)
+netflixactivity$Date <- lubridate::dmy(netflixactivity$Date)
 
 # A separate column that contains a title for season, title of the show and which episode
 netflixactivity_title <- netflixactivity %>% separate (col = Title, into = c("title", "season", "episode_title"), sep = ': ')
@@ -27,25 +28,28 @@ mynetflix <- epsidoes_binged
 
 
 # Analyzing the month where I binged waatched the most shows
-mynetflix$month20 <- months(mynetflix$Date, abbreviate = T)
+mynetflix$months <- months(mynetflix$Date, abbreviate = T)
 
 
-number_episodes_per_month_plot <- ggplot(aes(x = month20, y = n, color = n), data = mynetflix) +
-  geom_col(fill = c("#800080")) +
+# Plotting graph to anakyse which month I watched most shows  
+number_episodes_per_month_plot <- ggplot(aes(x = months, y = n, fill = n), data = mynetflix) +
+  scale_fill_gradient(low = "orange", high = "red")+
+  geom_bar(width=0.7, stat = "identity")
   theme_minimal() +
   ggtitle("Episodes watched per month", "duration from 2020 to 2021") +
   labs(x = "Month", y = "Number of Episodes watched") 
-number_episodes_per_day_plot
+number_episodes_per_month_plot
 
-#converting dates into days 
-number_episodes_per_day <- number_episodes_per_day[order(number_episodes_per_day$Date),]
-number_episodes_per_day$false_day <- wday(number_episodes_per_day$Date)
-number_episodes_per_day$days <- weekdays(number_episodes_per_day$Date, abbreviate = F)
+
+# Converting dates into days 
+mynetflix <- mynetflix[order(mynetflix$Date),]
+mynetflix$false_day <- wday(mynetflix$Date)
+mynetflix$days <- weekdays(mynetflix$Date, abbreviate = F)
+mynetflix
+
+#plotting a graph to anakyse which days I watched most shows  
+number_episodes_per_day <- mynetflix %>% ggplot(aes(x = days, y = n, fill = n)) +
+  scale_fill_gradient(low = "blue", high = "red")+
+  geom_bar(width=0.7, stat = "identity")
 number_episodes_per_day
-
-number_episodes_per_day <- number_episodes_per_day[order(number_episodes_per_day$Date),]
-number_episodes_per_day$false_day <- wday(number_episodes_per_day$Date)
-number_episodes_per_day$days <- weekdays(number_episodes_per_day$Date, abbreviate = F)
-number_episodes_per_day
-
 
